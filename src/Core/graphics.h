f3797e92d981106e7f8e38a39242833d6f0ef62d
Grave188 <grave63@gmail.com>
2021-10-12 19:51:42 +0300

    Add checing for world up boundary for clip tile drawing

--- src/Core/graphics.h
+++ src/Core/graphics.h
@@ -8,35 +8,39 @@
 class Graphics
 {
 public:
-	Graphics(const LevelBounds& bounds)
+	explicit Graphics(const LevelBound& bounds)
 		: bounds_(bounds)
 		, rec_{0, 0, static_cast<float>(bounds.tileSize), static_cast<float>(bounds.tileSize)}
 	{ }
 
-	void drawLevelFrame(int frameWidth = 5, int frameOffset = 5)
+	void drawLevelBackground(int frameWidth = 5, int frameOffset = 5) const
 	{
-		// draw level frame
-		
-		Rectangle worldBoundFrame{
+		// draw level background and frame
+		Rectangle levelBound
+		{
 			static_cast<float>(bounds_.levelToScreenPosX - frameOffset),
 			static_cast<float>(bounds_.levelToScreenPosY - frameOffset),
 			static_cast<float>(bounds_.width * bounds_.tileSize + frameOffset * 2),
-			static_cast<float>(bounds_.height * bounds_.tileSize + frameOffset * 2) };
+			static_cast<float>(bounds_.height * bounds_.tileSize + frameOffset * 2) 
+		};
 
 		float roundness = 0.07f;
-		DrawRectangleRounded(worldBoundFrame, roundness, frameWidth, LIGHTGRAY);
-		DrawRectangleRoundedLines(worldBoundFrame, roundness, 1, frameWidth, RED);
+		DrawRectangleRounded(levelBound, roundness, frameWidth, LIGHTGRAY);
+		DrawRectangleRoundedLines(levelBound, roundness, 1, frameWidth, RED);
 	}
 
-	void drawTile(const Tile& tile)
+	void drawTile(const Tile& tile, int worldX = 0, int worldY = 0)
 	{
-		rec_.x = static_cast<float>(bounds_.levelToScreenPosX + tile.getX() * bounds_.tileSize);
-		rec_.y = static_cast<float>(bounds_.levelToScreenPosY + tile.getY() * bounds_.tileSize);
-		DrawRectangleRounded(rec_, 0.5f, 1, tile.getColor());
-		DrawRectangleRoundedLines(rec_, 0.5f, 1, bounds_.tileSize / 15, BLACK);
+		if (worldY + tile.getY() >= 0)
+		{
+			rec_.x = static_cast<float>(bounds_.levelToScreenPosX + (worldX + tile.getX()) * bounds_.tileSize);
+			rec_.y = static_cast<float>(bounds_.levelToScreenPosY + (worldY + tile.getY()) * bounds_.tileSize);
+			DrawRectangleRounded(rec_, 0.5f, 1, tile.getColor());
+			DrawRectangleRoundedLines(rec_, 0.5f, 1, bounds_.tileSize / 15, BLACK);
+		}
 	}
 
 private:
+	const LevelBound bounds_;
 	Rectangle rec_;
-	const LevelBounds bounds_;
 };
