9c917b2864d3d1be6b29008f975edfb69fce6aec
Grave188 <grave63@gmail.com>
2021-10-15 11:52:37 +0300

    Add ability to player to fall down

--- src/Core/player.h
+++ src/Core/player.h
@@ -15,29 +15,20 @@ public:
     Player(Player&) = delete;
     Player(Player&&) = delete;
 
+    // Input
     void updateInput()
     {
-        int tmpX = x_;
-        int tmpY = y_;
+        tmpX_ = x_;
+        tmpY_ = y_;
 
         // movement
-        if (IsKeyPressed(KEY_A)) --tmpX;
-        if (IsKeyPressed(KEY_D)) ++tmpX;
-        if (IsKeyPressed(KEY_W)) --tmpY;
-        if (IsKeyPressed(KEY_S)) ++tmpY;
-
-        if (std::all_of(player_.begin(), player_.end(), 
-                [this, tmpX, tmpY](const auto& tile)
-                {
-                    return level_->resolveCollision(tmpX + tile.getX(), tmpY + tile.getY());
-                }))
-        {
-            x_ = tmpX;
-            y_ = tmpY;
-        }
+        if (IsKeyPressed(KEY_A)) --tmpX_;
+        if (IsKeyPressed(KEY_D)) ++tmpX_;
+        if (IsKeyPressed(KEY_W)) --tmpY_;
+        if (IsKeyPressed(KEY_S)) ++tmpY_;
         
-        if (IsKeyPressed(KEY_Q)) isGodMode_ = !isGodMode_;
 
+        if (IsKeyPressed(KEY_Q)) isGodMode_ = !isGodMode_;
         // change figure
         if (isGodMode_)
         {
@@ -51,23 +42,46 @@ public:
         }
     }
 
-    void updateGraphics(Graphics& graphics) const
+    // Movement
+    void updateMovement(const float dt)
+    {
+        if (std::all_of(
+                player_.begin(), 
+                player_.end(),
+                [=](const auto& tile)
+                {
+                    return level_->resolveCollision(static_cast<int>(tmpX_) + tile.getX(),
+                                                    static_cast<int>(tmpY_) + tile.getY());
+                }))
+        {
+            x_ = tmpX_;
+            y_ = tmpY_;
+        }
+
+        if (dt < 1.0f) y_ += speed_ * dt;
+        else           y_ += 1.0f;
+    }
+
+    // Graphics
+    void updateGraphics(const Graphics& graphics) const
     {
         for (const auto& tile : player_)
         {
-            graphics.drawTile(tile, x_, y_);
+            graphics.drawTile(tile, static_cast<int>(x_), static_cast<int>(y_));
         }
     }
 
-    bool isGodMode() const
-    {
-        return isGodMode_;
-    }
+    bool isGodMode() const { return isGodMode_; }
+    float getX() const { return x_; }
+    float getY() const { return y_; }
 
 private:
-    int x_ = 5;
-    int y_ = 3;
+    float x_ = 5.0f;
+    float y_ = 3.0f;
+    float tmpX_ = 0.0f;
+    float tmpY_ = 0.0f;
+    float speed_ = 1.0f;
+    bool isGodMode_ = true;
     std::array<Tile, 4> player_ = Figures::o;
     Level* level_;
-    bool isGodMode_ = true;
 };
--- src/Game.cpp
+++ src/Game.cpp
@@ -45,6 +45,7 @@ int main()
     while (!WindowShouldClose())
     {
         player.updateInput();
+        player.updateMovement(GetFrameTime());
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
