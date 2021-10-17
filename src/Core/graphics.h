61e1c5c692cf00078844f4236c33f33b7babfd80
Grave188 <grave63@gmail.com>
2021-10-17 13:12:13 +0300

    Add draw score for graphics functionality

--- src/Core/graphics.h
+++ src/Core/graphics.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <raylib.h>
+#include <string>
 #include "tile.h"
 #include "levelBounds.h"
 
@@ -8,25 +9,50 @@
 class GraphicsSystem
 {
 public:
-	explicit GraphicsSystem(const LevelBound& bounds)
-		: bounds_(bounds)
-		
+	GraphicsSystem(const LevelBound& levelBounds, const ScoreBound& scoreBound)
+		: levelBound_(levelBounds)
+		, scoreBound_(scoreBound)
 	{ }
 
-	void drawLevelBackground(int frameWidth = 5, int frameOffset = 5) const
+	void drawLevelBackground() const
 	{
+		int frameOffset = 5;
 		// draw level background and frame
 		Rectangle levelBoundRec
 		{
-			static_cast<float>(bounds_.levelToScreenPosX - frameOffset),
-			static_cast<float>(bounds_.levelToScreenPosY - frameOffset),
-			static_cast<float>(bounds_.width * bounds_.tileSize + frameOffset * 2),
-			static_cast<float>(bounds_.height * bounds_.tileSize + frameOffset * 2) 
+			static_cast<float>(levelBound_.windowToScreenPosX - frameOffset),
+			static_cast<float>(levelBound_.windowToScreenPosY - frameOffset),
+			static_cast<float>(levelBound_.windowWidthInTiles * levelBound_.tileSize + frameOffset * 2),
+			static_cast<float>(levelBound_.windowHeightInTiles * levelBound_.tileSize + frameOffset * 2) 
 		};
 
+		int frameWidth = 5;
 		float roundness = 0.07f;
-		DrawRectangleRounded(levelBoundRec, roundness, frameWidth, LIGHTGRAY);
+		DrawRectangleRounded(levelBoundRec, roundness, 1, LIGHTGRAY);
 		DrawRectangleRoundedLines(levelBoundRec, roundness, 1, frameWidth, RED);
+	}
+
+	void drawScore(int score) const
+	{
+		Rectangle scoreRec
+		{
+			static_cast<float>(scoreBound_.windowToScreenPosX),
+			static_cast<float>(scoreBound_.windowToScreenPosY),
+			static_cast<float>(scoreBound_.windowWidth),
+			static_cast<float>(scoreBound_.windowHeight)
+		};
+
+		float roundness = 0.2f;
+		int frameWidth = 5;
+		DrawRectangleRounded(scoreRec, roundness, 1, LIGHTGRAY);
+		DrawRectangleRoundedLines(scoreRec, roundness, 1, frameWidth, RED);
+
+		const std::string text = std::to_string(score);
+		const int scoreX = scoreBound_.scorePositionX + scoreBound_.windowToScreenPosX;
+		const int scoreY = scoreBound_.scorePositionY + scoreBound_.windowToScreenPosY;
+		const int fontSize = 100;
+		const Color fontColor = WHITE;
+		DrawText(text.c_str(), scoreX, scoreY, fontSize, fontColor);
 	}
 
 	void drawTile(const Tile& tile, int worldX = 0, int worldY = 0) const
@@ -35,19 +61,20 @@ public:
 		{
 			Rectangle tileRec
 			{
-				static_cast<float>(bounds_.levelToScreenPosX + (worldX + tile.getX()) * bounds_.tileSize),
-				static_cast<float>(bounds_.levelToScreenPosY + (worldY + tile.getY()) * bounds_.tileSize),
-				static_cast<float>(bounds_.tileSize),
-				static_cast<float>(bounds_.tileSize)
+				static_cast<float>(levelBound_.windowToScreenPosX + (worldX + tile.getX()) * levelBound_.tileSize),
+				static_cast<float>(levelBound_.windowToScreenPosY + (worldY + tile.getY()) * levelBound_.tileSize),
+				static_cast<float>(levelBound_.tileSize),
+				static_cast<float>(levelBound_.tileSize)
 			};
 
 			float roundness = 0.5f;
-			int lineThick = bounds_.tileSize / 15;
+			int lineThick = levelBound_.tileSize / 15;
 			DrawRectangleRounded(tileRec, roundness, 1, tile.getColor());
 			DrawRectangleRoundedLines(tileRec, roundness, 1, lineThick, BLACK);
 		}
 	}
 
 private:
-	const LevelBound bounds_;
+	const LevelBound levelBound_;
+	const ScoreBound scoreBound_;
 };
