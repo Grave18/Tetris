b412781de50f4fbf6a6ee341d45cd539690c6034
Grave188 <grave63@gmail.com>
2021-10-18 11:37:32 +0300

    Split declaration and definition of level

---
+++ src/Core/level.cpp
@@ -0,0 +1,127 @@
+#include "level.h"
+#include "player.h"
+#include "observer.h"
+
+// public:
+Level::Level(const LevelBound& bounds)
+    : bounds_(bounds)
+{
+    // temporary level fill
+    /*addTile(1, 7, GREEN);
+    addTile(9, 5, BLUE);*/
+}
+
+void Level::updateGraphics(const GraphicsSystem& graphics) const
+{
+    graphics.drawLevelBackground();
+
+    // draw level tiles
+    for (int i = 0; i < nextIndex_; ++i)
+    {
+        graphics.drawTile(level_[i]);
+    }
+}
+
+bool Level::willNotCollideWith(int x, int y) const
+{
+    // check level bounds
+    if (x < 0 || x >= bounds_.windowWidthInTiles || y >= bounds_.windowHeightInTiles)
+        return false;
+
+    // check each tile for collision
+    return std::none_of(level_.begin(), level_.begin() + nextIndex_,
+                        [x, y](const auto& tile)
+    {
+        return x == tile.getX() && y == tile.getY();
+    });
+}
+
+void Level::clear()
+{
+    nextIndex_ = 0;
+    rows_ = { 0 };
+    TraceLog(LOG_INFO, ("nextIndex = " + std::to_string(nextIndex_)).c_str());
+}
+
+void Level::onNotify(void* entity, Events event)
+{
+    if (event == Events::PLAYER_FELL)
+    {
+        auto player = static_cast<Player*>(entity);
+
+        for (const auto& tile : player->getTiles())
+        {
+            addTile(tile.getX() + player->getX(), tile.getY() + player->getY(), tile.getColor());
+        }
+
+        scanRows();
+    }
+}
+
+// private:
+void Level::scanRows()
+{
+    int i = rowsCount_ - 1;
+    while (i >= 0)
+    {
+        if (rows_[i] == 10)
+        {
+            clearRow(i);
+            TraceLog(LOG_INFO, "ROW_CLEARED_EVENT");
+            observers_.notify(this, Events::ROW_CLEARED);
+        }
+        else --i;
+    }
+}
+
+void Level::addTile(int x, int y, Color color)
+{
+    if (nextIndex_ != MAX_SIZE)
+    {
+        level_[nextIndex_].setTile(x, y, color);
+        ++nextIndex_;
+        ++rows_[y];
+
+        TraceLog(LOG_INFO, ("nextIndex = " + std::to_string(nextIndex_)).c_str());
+        TraceLog(LOG_INFO, ("row " + std::to_string(y) + " = "
+                            + std::to_string(rows_[y])).c_str());
+    }
+
+}
+
+void Level::clearRow(int row)
+{
+    int  currentIndex = 0;
+    while (currentIndex < nextIndex_)
+    {
+        // check if tile in the row and if true then throw away in the back
+        // of active part of array and decrement next index else procced with new index
+        if (level_[currentIndex].getY() == row)
+        {
+            --nextIndex_;
+            std::swap(level_[currentIndex], level_[nextIndex_]);
+            TraceLog(LOG_INFO, ("nextIndex = " + std::to_string(nextIndex_)).c_str());
+        }
+        else ++currentIndex;
+    }
+
+    shiftDownRows(row - 1);
+}
+
+void Level::shiftDownRows(int row)
+{
+    for (int i = 0; i < nextIndex_; ++i)
+        if (level_[i].getY() <= row)
+        {
+            level_[i].setY(level_[i].getY() + 1);
+        }
+
+    // refresh number of tiles in rows
+    rows_[0] = 0;
+    for (int i = row; i >= 0; --i)
+    {
+        rows_[i + 1] = rows_[i];
+        TraceLog(LOG_INFO, ("row " + std::to_string(i + 1) + " = "
+                            + std::to_string(rows_[i + 1])).c_str());
+    }
+}
--- src/Core/level.h
+++ src/Core/level.h
@@ -13,131 +13,22 @@
 class Level : public Observer
 {
 public:
-    explicit Level(const LevelBound& bounds)
-    :  bounds_(bounds)
-    {
-        // temporary level fill
-        /*addTile(1, 7, GREEN);
-        addTile(9, 5, BLUE);*/
-    }
+    explicit Level(const LevelBound& bounds);
     Level(Level&) = delete;
     Level(Level&&) = delete;
 
-    void updateGraphics(const GraphicsSystem& graphics) const
-    {
-        graphics.drawLevelBackground();
-
-        // draw level tiles
-        for (int i = 0; i < nextIndex_; ++i)
-        {
-            graphics.drawTile(level_[i]);
-        }
-    }
-
-    bool willNotCollideWith(int x, int y) const
-    {
-        // check level bounds
-        if (x < 0 || x >= bounds_.windowWidthInTiles || y >= bounds_.windowHeightInTiles)
-            return false;
-
-        // check each tile for collision
-        return std::none_of(level_.begin(), level_.begin() + nextIndex_,
-            [x, y](const auto& tile)
-            {
-                return x == tile.getX() && y == tile.getY();
-            });
-    }
-
-    void addFigure(const std::array<Tile, 4>& tiles, int x, int y)
-    {
-        for (const auto& tile : tiles)
-        {
-            addTile(tile.getX() + x, tile.getY() + y, tile.getColor());
-        }
-
-        scanRows();
-    }
-
-    void scanRows()
-    {
-        int i = rowsCount_ - 1;
-        while (i >= 0)
-        {
-            if (rows_[i] == 10)
-            {
-                clearRow(i);
-                TraceLog(LOG_INFO, "ROW_CLEARED_EVENT");
-                observers_.notify(this, Events::ROW_CLEARED);
-            }
-            else --i;
-        }
-    }
-
-    void addTile(int x, int y, Color color)
-    {
-        if (nextIndex_ != MAX_SIZE)
-        {
-            level_[nextIndex_].setTile(x, y, color);
-            ++nextIndex_;
-            ++rows_[y];
-
-            TraceLog(LOG_INFO, ("nextIndex = " + std::to_string(nextIndex_)).c_str());
-            TraceLog(LOG_INFO, ("row " + std::to_string(y) + " = "
-                                + std::to_string(rows_[y])).c_str());
-        }
-
-    }
-
-    void clearRow(int row)
-    {
-        int  currentIndex = 0;
-        while (currentIndex < nextIndex_)
-        {
-            // check if tile in the row and if true then throw away in the back
-            // of active part of array and decrement next index else procced with new index
-            if (level_[currentIndex].getY() == row)
-            {
-                --nextIndex_;
-                std::swap(level_[currentIndex], level_[nextIndex_]);
-                TraceLog(LOG_INFO, ("nextIndex = " + std::to_string(nextIndex_)).c_str());
-            }
-            else ++currentIndex;
-        }
-
-        shiftDownRows(row - 1);
-    }
-
-    void shiftDownRows(int row)
-    {
-        for (int i = 0; i < nextIndex_; ++i) 
-            if (level_[i].getY() <= row)
-            {
-                level_[i].setY(level_[i].getY() + 1);
-            }
-
-        // refresh number of tiles in rows
-        rows_[0] = 0;
-        for (int i = row; i >= 0; --i)
-        {
-            rows_[i + 1] = rows_[i];
-            TraceLog(LOG_INFO, ("row " + std::to_string(i + 1) + " = "
-                                + std::to_string(rows_[i + 1])).c_str());
-        }
-    }
-
-    void clear()
-    {
-        nextIndex_ = 0;
-        rows_ = { 0 };
-        TraceLog(LOG_INFO, ("nextIndex = " + std::to_string(nextIndex_)).c_str());
-    }
-
-    Subject& clearedRowEvent() { return observers_; }
+    void updateGraphics(const GraphicsSystem& graphics) const;
+    bool willNotCollideWith(int x, int y) const;
+    void clear();
 
     void onNotify(void* entity, Events event) override;
+    Subject& clearedRowEvent() { return observers_; }
 
 private:
-    LevelBound bounds_;
+    void scanRows();
+    void addTile(int x, int y, Color color);
+    void clearRow(int row);
+    void shiftDownRows(int row);
 
     // index of neft not occupied tile
     int nextIndex_ = 0;
@@ -148,5 +39,6 @@ private:
     static const int rowsCount_ = 20;
     std::array<int, rowsCount_> rows_ {0};
 
+    LevelBound bounds_;
     Subject observers_;
 };
