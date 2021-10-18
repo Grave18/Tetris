46a3dece9f3245c5437a5ae65088bae2546c6d11
Grave188 <grave63@gmail.com>
2021-10-18 13:22:57 +0300

    Add ranndom figure change

--- src/Core/player.h
+++ src/Core/player.h
@@ -8,6 +8,8 @@
 #include "soundSystem.h"
 #include "observer.h"
 
+using Figure = std::array<Tile, 4>;
+
 // player's default fall speed
 constexpr float DEFAULT_SPEED = 1.0f;
 constexpr float SPRINT_SPEED = 8.0f;
@@ -19,6 +21,8 @@ public:
     explicit Player(Level* level)
         : level_(level)
     {
+        player_ = nextFigureRandom();
+        nextFigure_ = nextFigureRandom();
         placePlayerToStartPosition();
     }
     Player(Player&) = delete;
@@ -35,7 +39,7 @@ public:
         if (IsKeyPressed(KEY_W))  tryToRotate();
 
 
-        // God Mode
+        // GodMode
         if (IsKeyPressed(KEY_Q)) isInGodMode_ = !isInGodMode_;
         if (isInGodMode_)
         {
@@ -68,23 +72,22 @@ public:
             tmpY += speed_ * dt;
         else           
             tmpY += 1.0f;
-
-        // check collision
-        bool isNotCollide = std::all_of(player_.begin(), player_.end(),
+        
+        bool isNotCollideWithWorld = std::all_of(player_.begin(), player_.end(),
                 [this, tmpY](const auto& tile)
                 {
                     return level_->willNotCollideWith(x_ + tile.getX(),
                                   static_cast<int>(tmpY) + tile.getY());
                 });
         
-        if (isNotCollide)
+        // check collision with world
+        if (isNotCollideWithWorld)
             y_ = tmpY;
         else
         {
-            // Fire fell event
-            observers_.notify(this, Events::PLAYER_FELL);
-            TraceLog(LOG_INFO, "FELL_EVENT");
-
+            firePlayerFellEvent();
+            player_ = nextFigure_;
+            nextFigure_ = nextFigureRandom();
             placePlayerToStartPosition();
         }
     }
@@ -101,7 +104,7 @@ public:
     int getX() const { return x_; }
     int getY() const { return static_cast<int>(y_); }
 
-    std::array<Tile, 4> getTiles() const { return player_; }
+    Figure getTiles() const { return player_; }
     Subject& fellEvent() { return observers_; }
 
 private:
@@ -112,13 +115,11 @@ private:
         if (side == "right") tmpX = x_ + 1;
 
         // check collision
-        if (std::all_of(
-            player_.begin(),
-            player_.end(),
-            [this, tmpX](const auto& tile)
-        {
-            return level_->willNotCollideWith(tmpX + tile.getX(), static_cast<int>(y_) + tile.getY());
-        }))
+        if (std::all_of( player_.begin(), player_.end(),
+                [this, tmpX](const auto& tile)
+                {
+                    return level_->willNotCollideWith(tmpX + tile.getX(), static_cast<int>(y_) + tile.getY());
+                }))
         {
             x_ = tmpX;
         }
@@ -128,15 +129,14 @@ private:
     void tryToRotate()
     {
         // rotate if new position of player didn't collide with level
-        if (std::all_of(
-            player_.begin(),
-            player_.end(),
-            [this](const auto& tile)
-        {
-            const int tileX = tile.getY();
-            const int tileY = -tile.getX();
-            return level_->willNotCollideWith(x_ + tileX, static_cast<int>(y_) + tileY);
-        }))
+        if (std::all_of(player_.begin(), player_.end(),
+                [this](const auto& tile)
+                {
+                    const int tileX = tile.getY();
+                    const int tileY = -tile.getX();
+                    return level_->willNotCollideWith(x_ + tileX,
+                                    static_cast<int>(y_) + tileY);
+                }))
         {
             for (auto& tile : player_)
             {
@@ -148,18 +148,58 @@ private:
         }
     }
 
+    void firePlayerFellEvent()
+    {
+        observers_.notify(this, Events::PLAYER_FELL);
+        TraceLog(LOG_INFO, "PLAYER_FELL");
+    }
+
     void placePlayerToStartPosition()
     {
         x_ = 5;
         y_ = 3.0f;
+    }
+    
+    [[nodiscard]]
+    Figure& nextFigureRandom()
+    {
+        int random = GetRandomValue(1, 7);
+
+        switch (random)
+        {
+        case 1:
+            return Figures::o;
+            break;
+        case 2:
+            return Figures::i;
+            break;
+        case 3:
+            return Figures::s;
+            break;
+        case 4:
+            return Figures::z;
+            break;
+        case 5:
+            return Figures::l;
+            break;
+        case 6:
+            return Figures::j;
+            break;
+        case 7:
+            return Figures::t;
+            break;
+
+        default:
+            return Figures::o;
+        }
     }
 
     int x_ = 0;
     float y_ = 0.0f;
     float speed_ = DEFAULT_SPEED;
     bool isInGodMode_ = true;
-    std::array<Tile, 4> player_ = Figures::j;
     Level* level_;
-
+    Figure player_;
+    Figure nextFigure_;
     Subject observers_;
 };
