f46603806e247c5d42609cebed95600e38fbfa83
Grave188 <grave63@gmail.com>
2021-10-21 22:40:59 +0300

    Add ability to regulate sound volume

--- src/Core/soundSystem.h
+++ src/Core/soundSystem.h
@@ -7,33 +7,49 @@
 class SoundSystem : public Observer
 {
 public:
-	SoundSystem()
-	{
-		InitAudioDevice();
-		playerFell = LoadSound("resources/player_fell.wav");
-		clearRow = LoadSound("resources/clear_row.wav");
-	}
-	SoundSystem(SoundSystem&) = delete;
-	SoundSystem(SoundSystem&&) = delete;
-	~SoundSystem() override
-	{
-		CloseAudioDevice();
-		UnloadSound(playerFell);
-		UnloadSound(clearRow);
-	}
+    SoundSystem()
+    {
+        InitAudioDevice();
+        sounds_.push_back(LoadSound("resources/player_fell.wav"));
+        sounds_.push_back(LoadSound("resources/clear_row.wav"));
+    }
+    SoundSystem(SoundSystem&) = delete;
+    SoundSystem(SoundSystem&&) = delete;
+    ~SoundSystem() override
+    {
+        for (const auto& sound : sounds_)
+        {
+            UnloadSound(sound);
+        }
+        CloseAudioDevice();
+    }
  
-	void onNotify(void* entity, Events event) override
-	{
-		if (event == Events::ROW_CLEARED)
-			playSoundClearRow();
-		else if (event == Events::PLAYER_FELL)
-			playSoundPlayerFell();
-	}
+    void onNotify(void* entity, Events event) override
+    {
+        if (event == Events::ROW_CLEARED)
+            playSoundClearRow();
+        else if (event == Events::PLAYER_FELL)
+            playSoundPlayerFell();
+    }
 
-	void playSoundPlayerFell() const { PlaySound(playerFell); }
-	void playSoundClearRow() const { PlaySound(clearRow); }
+    void playSoundPlayerFell() const { PlaySound(sounds_[0]); }
+    void playSoundClearRow() const { PlaySound(sounds_[1]); }
+
+    void setSoundVolume(float volume) 
+    {
+        if (volume > 1.0f) soundVolume_ = 1.0f;
+        else if (volume < 0.0f) soundVolume_ = 0.0f;
+        else soundVolume_ = volume;
+
+        for (auto& sound : sounds_)
+        {
+            SetSoundVolume(sound, soundVolume_);
+        }
+    }
+    float getSoundVolume() const { return soundVolume_; }
 
 private:
-	Sound playerFell;
-	Sound clearRow;
+    float soundVolume_ = 0.5f;
+
+    std::vector<Sound> sounds_;
 };
